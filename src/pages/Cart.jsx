7013b23d53f5756e994a40bedde8a17d7b35339e
Ali Can Yelekci <acyelekci@gmail.com>
2023-12-24 00:03:56 +0300

    styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,27 +9,43 @@ export default function Cart() {
   }, 0);
 
   return (
-    <>
-      {total !== 0 ? <div onClick={() => emptyCart()}>EMPTY CART</div> : <></>}
+    <div className="cart-container">
+      {total === 0 ? (
+        <div className="empty-cart" onClick={() => emptyCart()}>
+          Looks like your cart is empty!
+        </div>
+      ) : (
+        <></>
+      )}
       {cartList.map((item) => {
         return (
           <div key={item.id} className="cart-item">
-            <div>{item.title}</div>
+            <div className="title">{item.title}</div>
             <img src={item.image} />
-            <div>${item.price}</div>
-            <div>{item.count}</div>
-            <div onClick={() => increaseCart(item)}>+</div>
-            {item.count === 1 ? (
-              <div onClick={() => decreaseCart(item)}>Remove</div>
-            ) : (
-              <div onClick={() => decreaseCart(item)}>-</div>
-            )}
-            <div>Subtotal = ${(item.price * item.count).toFixed(2)}</div>
+            <div className="price">${item.price}</div>
+            <div className="count">
+              <div className="quantity">{item.count}</div>
+              <div className="increase" onClick={() => increaseCart(item)}>
+                +
+              </div>
+              {item.count === 1 ? (
+                <div className="decrease" onClick={() => decreaseCart(item)}>
+                  Remove
+                </div>
+              ) : (
+                <div className="decrease" onClick={() => decreaseCart(item)}>
+                  -
+                </div>
+              )}
+            </div>
+            <div className="subtotal">
+              Subtotal = ${(item.price * item.count).toFixed(2)}
+            </div>
           </div>
         );
       })}
       {total !== 0 ? <div>TOTAL = ${total.toFixed(2)}</div> : <></>}
       {total !== 0 ? <div>CHECKOUT</div> : <></>}
-    </>
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <h2>Your Cart, Your Gadgets</h2>
-      <p>
-        This is a mock shopping cart project done as a React project. Enjoy!
-      </p>
+    <div className="home">
+      <div className="container">
+        <h2>Your Cart, Your Gadgets</h2>
+        <p>
+          This is a mock shopping cart project done as a React project. All of
+          the items in this store are fetched from Fake Store API.
+        </p>
+      </div>
     </div>
   );
 }
